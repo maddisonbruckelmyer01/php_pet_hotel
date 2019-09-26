3d5deaa9d88d9a6222619471938f0381bf454e83
Miles Lacek <mileslacek@gmail.com>
2019-09-26 13:10:36 -0500

    Added SQL INSERT and SELECT to database.sql file.

--- database.sql
+++ database.sql
@@ -11,3 +11,32 @@ CREATE TABLE "owners" (
 	"id" SERIAL PRIMARY KEY,
 	"name" VARCHAR(100)
 );
+
+
+INSERT INTO "pets"
+	("pet_name", "breed", "color", "owner_name")
+VALUES
+	('Stuart', 'Hamster', 'White', '2');
+--ADDS A PET INTO THE DATABASE--
+
+INSERT INTO "owners"
+	("name")
+VALUES
+	('Miles'),
+	('Maddison'),
+	('Dustin'),
+	('Andy');
+--ADDS AN OWNER INTO THE DATABASE-- 
+
+SELECT *
+FROM "pets"
+	JOIN "owners" ON "pets".owner_name = "owners".id;
+--GETS THE NUMBER OF PETS PER THE OWNER--
+
+SELECT *
+FROM "pets";
+--GETS ALL THE PETS--
+
+SELECT *
+FROM "owners";
+--GETS ALL THE OWNERS--
