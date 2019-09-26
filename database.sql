71a51265370e3c4b93cc7ccef10405c16aaf656d
Miles Lacek <mileslacek@gmail.com>
2019-09-26 16:12:58 -0500

    Added SQL for GET PETS.

--- database.sql
+++ database.sql
@@ -33,10 +33,14 @@ FROM "pets"
 	JOIN "owners" ON "pets".owner_name = "owners".id;
 --GETS THE NUMBER OF PETS PER THE OWNER--
 
-SELECT *
-FROM "pets";
---GETS ALL THE PETS--
+SELECT "owners".name, "owners".id, COUNT("owner_name")
+FROM "pets"
+	JOIN "owners" ON "owners".id = "pets".owner_name
+GROUP BY "owners".id;
+--GETS THE COUNT OF PETS PER THE ID OF EACH USER--
 
-SELECT *
-FROM "owners";
---GETS ALL THE OWNERS--
+SELECT "owners".name AS "owner_name", "pets".id, "pets".breed, "pets".checked_in, "pets".color, "pets".pet_name
+FROM "owners"
+	JOIN "pets" ON "owners".id = "pets".owner_name
+GROUP BY "pets".id, "owners".name;
+--GETS THE PET SAGA 
